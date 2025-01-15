5725e246ecd46759875a0b717191fe44468ff2f4
Bernard-Mangulabnan <adaiki287@gmail.com>
2025-01-16 00:59:36 +0800

    try

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,24 @@
 import Link from "next/link";
+const mocksUrls = [
+  "https://www.cnet.com/a/img/resize/bd44a86a4ba4720c7f8f85ffaa139e051ac2e2cb/hub/2019/05/29/9e668aa2-f32e-4202-bb35-4ef8988b3557/2020-ferrari-sf90-stradale-01.jpg?auto=webp&width=1200"
+];
+const mocksImages = mocksUrls.map((url, index)=>({
+id: index +1,
+url,
+}));
 
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      Hello (Gallery )
-      t
+    <main className="">
+     <div className="flex flex-wrap gap-4">
+      {mocksImages.map((image)=>(
+        <div key={image.id} className="w-48">
+          <img src={image.url}/>
+
+          </div>
+     ))}
+      
+  </div>
     </main>
   );
 }
