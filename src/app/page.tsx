c45484f7bbe3bcfddf637b71a9258f9473eff616
Bernard-Mangulabnan <adaiki287@gmail.com>
2025-01-16 02:23:36 +0800

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
+import Link from "next/link";
 import Image from "next/image";
 
 const mocksUrls = [
-  "https://www.cnet.com/a/img/resize/bd44a86a4ba4720c7f8f85ffaa139e051ac2e2cb/hub/2019/05/29/9e668aa2-f32e-4202-bb35-4ef8988b3557/2020-ferrari-sf90-stradale-01.jpg?auto=webp&width=1200"
-  ,"https://mxatayqbwx.ufs.sh/f/PwsLPXIQSutRXPsSHRoP7IfZc2LMnY4Gmhdp8zObCS1XJVat",
+  "https://www.cnet.com/a/img/resize/bd44a86a4ba4720c7f8f85ffaa139e051ac2e2cb/hub/2019/05/29/9e668aa2-f32e-4202-bb35-4ef8988b3557/2020-ferrari-sf90-stradale-01.jpg?auto=webp&width=1200",
+  "https://mxatayqbwx.ufs.sh/f/PwsLPXIQSutRXPsSHRoP7IfZc2LMnY4Gmhdp8zObCS1XJVat",
   "https://mxatayqbwx.ufs.sh/f/PwsLPXIQSutR0lx6hG7NXoD3LRYx4gu25hbs9QVBPCEI7yfT",
   "https://mxatayqbwx.ufs.sh/f/PwsLPXIQSutRWI8gtM06ABbsZl8uCJIVg4SOGR7xr5Dd3kFL",
   "https://mxatayqbwx.ufs.sh/f/PwsLPXIQSutRwqa2Vk9mWFuHvrdgnPkb1NMsLBlhUXSoE89T",
@@ -15,12 +16,20 @@ const mocksUrls = [
   "https://mxatayqbwx.ufs.sh/f/PwsLPXIQSutRJvDZYau2cqsG9Igepjb1vQzrOAU0ti3uyXl7",
   "https://mxatayqbwx.ufs.sh/f/PwsLPXIQSutR77xR5Pg54HL3SyQOxgcdluKkomVjriqTv9P2",
   "https://mxatayqbwx.ufs.sh/f/PwsLPXIQSutRKDYVm6X8kLTRVqaJOEfSBZHY4QvGhy92Uw5z",
-  "https://mxatayqbwx.ufs.sh/f/PwsLPXIQSutRWBLZG506ABbsZl8uCJIVg4SOGR7xr5Dd3kFL",
-  "https://mxatayqbwx.ufs.sh/f/PwsLPXIQSutRY4tR3olVnTiEXQMj1qLgy9vsl5pbUW3D7fBP",
-  "https://mxatayqbwx.ufs.sh/f/PwsLPXIQSutRocqfLdzMcIlGB9ACk03Yyh2qjtfT4vDJEg68",
-  "https://mxatayqbwx.ufs.sh/f/PwsLPXIQSutRFB4MZucK0QeEqUDxv1kNtAyVBi5b384WZmXs",
-
+  "https://images.pexels.com/photos/3426021/pexels-photo-3426021.jpeg",
+  "https://images.pexels.com/photos/2988690/pexels-photo-2988690.jpeg",
+  "https://images.pexels.com/photos/1814842/pexels-photo-1814842.jpeg",
+  "https://images.pexels.com/photos/1108090/pexels-photo-1108090.jpeg",
+  "https://images.pexels.com/photos/2164847/pexels-photo-2164847.jpeg",
+  "https://images.pexels.com/photos/3488594/pexels-photo-3488594.jpeg",
+  "https://images.pexels.com/photos/1804485/pexels-photo-1804485.jpeg",
+  "https://images.pexels.com/photos/2743503/pexels-photo-2743503.jpeg",
+  "https://images.pexels.com/photos/2376750/pexels-photo-2376750.jpeg",
+  "https://images.pexels.com/photos/3144655/pexels-photo-3144655.jpeg",
+  "https://images.pexels.com/photos/3225510/pexels-photo-3225510.jpeg",
+  "https://images.pexels.com/photos/1560140/pexels-photo-1560140.jpeg"
 ];
+
 
 const mocksImages = mocksUrls.map((url, index) => ({
   id: index + 1,
@@ -30,21 +39,37 @@ const mocksImages = mocksUrls.map((url, index) => ({
 export default function HomePage() {
   return (
     <main>
-      <div className="flex flex-wrap gap-4"/>
-      <h1 className="text-center text-2xl font-bold my-4">All Photos</h1>
-      <div className="flex flex-wrap gap-4">
-      {mocksImages.map((image) => (
+      <div className="flex justify-center gap-4 mb-6">
+        {/* Button for "All Photos" */}
+        <Link href="/" passHref>
+          <button className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-700">
+            All Photos
+          </button>
+        </Link>
+
+        {/* Button for "Favorites" */}
+        <Link href="/favorites.tsx" passHref>
+          <button className="px-4 py-2 bg-green-500 text-white rounded-lg hover:bg-green-700">
+            Favorites
+          </button>
+        </Link>
+      </div>
+
+      <div className="flex flex-wrap gap-4 justify-center">
+        {mocksImages.map((image) => (
           <div
             key={image.id}
-            className="w-48 h-48 overflow-hidden border-2 border-black"
+            className="w-48 h-48 overflow-hidden border-2 border-black cursor-pointer"
           >
-            <img
-              src={image.url}
-              alt={`Image ${image.id}`}
-              width={562}
-              height={352}
-              className="object-cover w-full h-full"
-            />
+          
+              <img
+                src={image.url}
+                alt={`Image ${image.id}`}
+                width={192}
+                height={192}
+                className="object-cover w-full h-full"
+              />
+          
           </div>
         ))}
       </div>
